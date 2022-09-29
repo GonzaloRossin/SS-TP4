a06614fbb9dad6589796d4e3c998cd123beb5c8c
holgidonath <hdonath@itba.edu.ar>
2022-09-29 13:46:45 -0300

    added plots ej1

---
+++ plots/ej1.py
@@ -0,0 +1,23 @@
+import matplotlib.pylab as plt
+
+def mov_oscilador(timeAnalytical, posAnalytical, timeVerlet, posVerlet, timeBeeman, posBeeman, timeGCP, posGCP):
+    plt.plot(timeAnalytical, posAnalytical, 'b-', label="Solución Analitica")
+    plt.plot(timeVerlet, posVerlet, 'g--', label="Verlet")
+    plt.plot(timeBeeman, posBeeman, 'r-.', label="Beeman")
+    plt.plot(timeGCP, posGCP, 'k:', label="Gear predictor-corrector")
+    plt.legend()
+    plt.xlabel("Tiempo (s) ", fontsize=16)
+    plt.ylabel("Posición del péndulo (m)", fontsize=16)
+    plt.show()
+
+
+def error(deltaVerlet, errorVerlet, deltaBeeman, errorBeeman, deltaGCP, errorGCP):
+    plt.plot(deltaVerlet, errorVerlet, 'b-o', label="Verlet")
+    plt.plot(deltaBeeman, errorBeeman, 'r-o', label="Beeman")
+    plt.plot(deltaGCP, errorGCP, 'g-o', label="Gear predictor-corrector")
+    plt.legend()
+    plt.xlabel("Δt (s) ", fontsize=16)
+    plt.ylabel("Error cuadrático medio (m²)", fontsize=16)
+    plt.xscale("log")
+    plt.yscale("log")
+
