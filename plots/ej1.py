64df6e2c63b7b0b768c2be202ab36a86507f416f
holgidonath <hdonath@itba.edu.ar>
2022-10-03 12:24:20 -0300

    plots parameters simplification

--- plots/ej1.py
+++ plots/ej1.py
@@ -2,21 +2,21 @@ import matplotlib.pylab as plt
 import pandas as pd
 
 
-def mov_oscilador(timeAnalytical, posAnalytical, timeVerlet, posVerlet, timeBeeman, posBeeman, timeGCP, posGCP):
-    plt.plot(timeAnalytical, posAnalytical, 'b-', label="Solución Analitica")
-    plt.plot(timeVerlet, posVerlet, 'g--', label="Verlet")
-    plt.plot(timeBeeman, posBeeman, 'r-.', label="Beeman")
-    plt.plot(timeGCP, posGCP, 'k:', label="Gear predictor-corrector")
+def mov_oscilador(time, posAnalytical, posVerlet, posBeeman, posGCP):
+    plt.plot(time, posAnalytical, 'b-', label="Solución Analitica")
+    plt.plot(time, posVerlet, 'g--', label="Verlet")
+    plt.plot(time, posBeeman, 'r-.', label="Beeman")
+    plt.plot(time, posGCP, 'k:', label="Gear predictor-corrector")
     plt.legend()
     plt.xlabel("Tiempo (s) ", fontsize=16)
     plt.ylabel("Posición del péndulo (m)", fontsize=16)
     plt.show()
 
 
-def error(deltaVerlet, errorVerlet, deltaBeeman, errorBeeman, deltaGCP, errorGCP):
-    plt.loglog(deltaVerlet, errorVerlet, 'b-o', label="Verlet")
-    plt.loglog(deltaBeeman, errorBeeman, 'r-o', label="Beeman")
-    plt.loglog(deltaGCP, errorGCP, 'g-o', label="Gear predictor-corrector")
+def error(deltaT, errorVerlet, errorBeeman, errorGCP):
+    plt.loglog(deltaT, errorVerlet, 'b-o', label="Verlet")
+    plt.loglog(deltaT, errorBeeman, 'r-o', label="Beeman")
+    plt.loglog(deltaT, errorGCP, 'g-o', label="Gear predictor-corrector")
     plt.legend()
     plt.xlabel("Δt (s) ", fontsize=16)
     plt.ylabel("Error cuadrático medio (m²)", fontsize=16)
@@ -37,5 +37,5 @@ pGCP = df['pGCP']
 pVerlet = df['pVerlet']
 x = df['time']
 
-mov_oscilador(x, pAnalytical, x, pVerlet, x, pBeeman, x, pGCP)
-error(delta, errorVerlet, delta, errorBeeman, delta, errorGCP)
+mov_oscilador(x, pAnalytical, pVerlet, pBeeman, pGCP)
+error(delta, errorVerlet, errorBeeman, errorGCP)
